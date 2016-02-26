5cee6a65a7c484eac600e8e9615eb564867c1608
Alexey Smirnov <chemistmail@gmail.com>
2016-02-26 18:37:08 +0300

    it can be simple

--- src/Pipes/BZip.hs
+++ src/Pipes/BZip.hs
@@ -12,36 +12,22 @@ import           Codec.Compression.BZip (CompressParams (..),
                                          defaultCompressParams,
                                          defaultDecompressParams)
 import qualified Codec.Compression.BZip as BZip
-import           Control.Monad          (forever)
 import           Data.ByteString        (ByteString)
-import qualified Data.ByteString        as B
 import qualified Data.ByteString.Lazy   as L
 import           Pipes
-import           Pipes.ByteString
+import qualified Pipes.Prelude          as P
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+process :: Monad m => (L.ByteString -> L.ByteString) -> Pipe ByteString ByteString m ()
+process f = P.map (L.toStrict . f . L.fromStrict)
 
 decompress :: Monad m => Pipe ByteString ByteString m ()
-decompress = forever $ do
-    bs <- await
-    let d = BZip.decompress (L.fromStrict bs)
-    yield $ L.toStrict d
+decompress = process BZip.decompress
 
 compress :: Monad m => Pipe ByteString ByteString m ()
-compress = forever $ do
-    bs <- await
-    let d = BZip.compress (L.fromStrict bs)
-    yield $ L.toStrict d
+compress = process BZip.compress
 
 decompressWith :: Monad m => DecompressParams -> Pipe ByteString ByteString m ()
-decompressWith p = forever $ do
-    bs <- await
-    let d = BZip.decompressWith p (L.fromStrict bs)
-    yield $ L.toStrict d
+decompressWith p = process (BZip.decompressWith p)
 
 compressWith :: Monad m => CompressParams -> Pipe ByteString ByteString m ()
-compressWith p = forever $ do
-    bs <- await
-    let d = BZip.compressWith p (L.fromStrict bs)
-    yield $ L.toStrict d
+compressWith p = process (BZip.compressWith p)
